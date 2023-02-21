0ad68ac9eebc974dbb08999cd53aa8676ffefbe5
FaisalMS <fis445@hotmail.com>
2023-02-22 00:02:03 +0300

    Verson 1

---
+++ pages/index.tsx
@@ -0,0 +1,27 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title> Faisal Portfolio </title>
+      </Head>
+      
+      {/* Header */}
+
+      {/* Hero */}
+
+      {/* About */}
+
+      {/* Experience */}
+
+      {/* Skills */}
+
+      {/* Contact Me */}
+    </div>  
+  )
+}
+
+export default Home
