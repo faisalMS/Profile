9de284b72aa6ea5c7c1a5707bd5c198e741c0345
FaisalMS <fis445@hotmail.com>
2023-02-23 09:04:32 +0300

    Version 2

---
+++ components/Hero.tsx
@@ -0,0 +1,60 @@
+import Link from 'next/link'
+import React from 'react'
+import { Cursor, useTypewriter } from 'react-simple-typewriter'
+import BackgroundCircles from './BackgroundCircles'
+import { motion } from "framer-motion";
+
+
+type Props = {}
+
+export default function Hero({}: Props) {
+    const [text, count] = useTypewriter({
+        words: [
+
+            "Hi, The name's Faisal AL Sofyani",
+            "Guy-who-loves-Coffee.tsx",
+            "<ButLovesToCodeMore />",
+        ],
+        loop: true,
+        delaySpeed: 2000,
+    })
+
+  return (
+    <div className='h-screen flex flex-col space-y-8 item-center justify-center
+    text-center overflow-hidden'>
+        <BackgroundCircles />
+
+        <motion.img
+        initial={{x: -200, opacity: 0,}}
+        transition={{ duration: 1.2 }}
+        whileInView={{ opacity: 1, x: 0 }}
+        viewport={{ once: true }}
+        className='relative rounded-full h-32 w-32 mx-auto object-cover'
+        src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTt2eBS-Fbb5zt-h0_3XuAVS4qNMJR09WrtfQ&usqp=CAU'
+        alt=''/>
+        <div className='z-20'>
+        <h2 className='text-sm uppercase text-gray-500 pb-2 tracking-[15px]'>Software Engineer</h2>
+            <h1 className='text-5xl lg:text-6xl font-semibold px-10'>
+                <span className='mr-3'> {text} </span>
+                <Cursor cursorColor='#F7AB0A' />
+            </h1>
+        </div>
+    <motion.div
+    initial={{x: -200, opacity: 0,}}
+    transition={{ duration: 1.2 }}
+    whileInView={{ opacity: 1, x: 0 }}
+    viewport={{ once: true }}
+    className='pt-5'>
+        <Link href='#about'>
+        <button className='heroButton'>About</button>
+        </Link>
+        <Link href='#experience'>
+        <button className='heroButton'>Experience</button>
+        </Link>
+        <Link href='#skills'>
+        <button className='heroButton'>Skills</button>
+        </Link>
+    </motion.div>
+</div>
+  )
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,26 +1,35 @@
 import type { NextPage } from 'next'
+import Header from '@/components/Header'
 import Head from 'next/head'
+import Hero from "../components/Hero"
+import About from '@/components/About'
 
 
 const Home: NextPage = () => {
   return (
-    <div>
+    <div className='bg-[rgb(0,0,0)] text-white h-screen snap-y snap-mandatory
+    overflow-scroll z-0'>
       <Head>
         <title> Faisal Portfolio </title>
       </Head>
-      
+
       {/* Header */}
+      <Header />
 
       {/* Hero */}
-
+      <section id='hero' className='snap-start'>
+        <Hero />
+      </section>
       {/* About */}
-
+      <section id='about' className='snap-center'>
+        <About />
+      </section>
       {/* Experience */}
 
       {/* Skills */}
 
       {/* Contact Me */}
-    </div>  
+    </div>
   )
 }
 
